6f41adb61498e9a6c30bd02938d9de7f7285d062
nobody <nobody@email.com>
2015-02-06 18:18:33 +0900

    2015-02-06

--- app/src/main/java/com/example/android/withoutprovider/FamilyContract.java
+++ app/src/main/java/com/example/android/withoutprovider/FamilyContract.java
@@ -4,6 +4,7 @@ import android.provider.BaseColumns;
 
 /**
  * Created by nobody on 2015-02-05.
+ *
  */
 public final class FamilyContract {
     private String TAG = "FamilyContract";
@@ -14,6 +15,10 @@ public final class FamilyContract {
 
     public class FamilyEntry implements BaseColumns {
         public static final String TABLE_NAME = "family";
+
+        public static final String COLUMN_NAME_NAME = "name";
+        public static final String COLUMN_NAME_GENDER = "gender";
+        public static final String COLUMN_NAME_BIRTHDAY = "birthday";
     }
 
 
--- app/src/main/java/com/example/android/withoutprovider/FamilyDbHelper.java
+++ app/src/main/java/com/example/android/withoutprovider/FamilyDbHelper.java
@@ -3,17 +3,34 @@ package com.example.android.withoutprovider;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by nobody on 2015-02-05.
  */
 public class FamilyDbHelper extends SQLiteOpenHelper {
+    private final String TAG = "FamilyDbHelper";
+
+    private static final String DATABASE_NAME = "withoutProvider.db";
+    private static final int DATABASE_VERSION = 1;
+
     public FamilyDbHelper(Context context) {
-        super(context, name, factory, version);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        Log.d(TAG, "FamilyDbHelper()");
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        Log.d(TAG, "onCreate()");
+        db.execSQL(
+                "create table " + FamilyContract.FamilyEntry.TABLE_NAME + " (" +
+                        FamilyContract.FamilyEntry._ID + " integer primary key, " +
+                        FamilyContract.FamilyEntry.COLUMN_NAME_NAME + " text, " +
+                        FamilyContract.FamilyEntry.COLUMN_NAME_GENDER + " text, " +
+                        FamilyContract.FamilyEntry.COLUMN_NAME_BIRTHDAY + " integer, " +
+                        ");"
+        );
+
 
     }
 
