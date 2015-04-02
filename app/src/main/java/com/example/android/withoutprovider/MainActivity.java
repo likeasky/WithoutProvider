62937b4617cf790cce3e861502ad094363388a19
nobody <nobody@email.com>
2015-04-02 18:57:39 +0900

    2015-03-11

--- app/src/main/java/com/example/android/withoutprovider/MainActivity.java
+++ app/src/main/java/com/example/android/withoutprovider/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.withoutprovider;
 
 import android.app.ListActivity;
 import android.app.LoaderManager;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -67,14 +68,16 @@ public class MainActivity extends ListActivity implements LoaderManager.LoaderCa
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                Intent intent = new Intent(####);
+                startActivity(intent);
+                return true;
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
